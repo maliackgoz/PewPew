83dc40d10bd9d67ea444c4da4bb383945928a5be
maliackgoz <98320395+maliackgoz@users.noreply.github.com>
2022-07-19 01:37:02 +0300

    Added Spawner.
    Will deal with the console error.

--- Assets/Scripts/AstreoidSpawner.cs
+++ Assets/Scripts/AstreoidSpawner.cs
@@ -1,18 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AstreoidSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public AstreoidController asteroidPrefab;
+    public float spawnDistance = 20f;
+    public float spawnRate = 1f;
+    public int amountPerSpawn = 1;
+    [Range(0f, 45f)]
+    public float trajectoryVariance = 15f;
+
+    private void Start()
     {
-        
+        InvokeRepeating(nameof(Spawn), this.spawnRate, this.spawnRate);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Spawn()
     {
-        
+        for (int i = 0; i < amountPerSpawn; i++)
+        {
+            // Choose a random direction from the center of the spawner and
+            // spawn the asteroid a distance away
+            Vector2 spawnDirection = Random.insideUnitCircle.normalized;
+            Vector3 spawnPoint = spawnDirection * spawnDistance;
+
+            // Offset the spawn point by the position of the spawner so its
+            // relative to the spawner location
+            spawnPoint += transform.position;
+
+            // Calculate a random variance in the asteroid's rotation which will
+            // cause its trajectory to change
+            float variance = Random.Range(-trajectoryVariance, trajectoryVariance);
+            Quaternion rotation = Quaternion.AngleAxis(variance, Vector3.forward);
+
+            // Create the new asteroid by cloning the prefab and set a random
+            // size within the range
+            AstreoidController asteroid = Instantiate(this.asteroidPrefab, spawnPoint, rotation);
+            asteroid.size = Random.Range(asteroid.minSize, asteroid.maxSize);
+
+            // Set the trajectory to move in the direction of the spawner
+            Vector2 trajectory = rotation * -spawnDirection;
+            asteroid.SetTrajectory(trajectory);
+        }
     }
+
 }
